568fc7e156d0bb35f71be255c0c79fbdd27ca9fb
Puneet Puri <puneet.puri999@gmail.com>
2025-10-21 16:12:25 -0400

    The start script runs backend.js but the actual file is named backend-margaret.js

--- backend/backend-margaret.js
+++ backend/backend-margaret.js
@@ -1,3 +1,4 @@
+//The Start script in package.json runs backend.js but the actual file is backend-margaret.js. Fixed package.json
 import express from "express";
 import {connectDB} from "./db/db.js"; // Addding from utkarsh's
 
